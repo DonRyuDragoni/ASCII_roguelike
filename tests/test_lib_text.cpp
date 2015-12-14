357a87a0bcc47ce560e2d3170ece99bc319be080
DonRyuDragoni <dragoni.ryu@gmail.com>
2015-12-14 13:04:14 -0200

    Small change in ducumentation

--- tests/test_lib_text.cpp
+++ tests/test_lib_text.cpp
@@ -8,7 +8,8 @@ Centered text will have no whitespace after it. So, if we pass the string "lhs",
 for example, the function should return "   lhs" instead of "   lhs   ", if the
 field has nine characters.
 
-Why? This was easier to make. :P
+Why? This was easier to make. :P Besides, when printing to the console, extra
+whitespace after the text will make no diference at all.
 */
 BOOST_AUTO_TEST_CASE(simple_centered_text) {
     BOOST_CHECK_EQUAL(txt::centerTextLine("lhs", 9),
